5d595b74faafa2e4e8f953ada75a025f029b1f60
Nikita Prokopenko <guardprokopenko@gmail.com>
2018-03-30 22:40:10 +0300

    Удалил localStorage

--- js/script.js
+++ js/script.js
@@ -5,10 +5,6 @@ var form = modal.querySelector("form");
 var login = modal.querySelector("[name=login]");
 var email = modal.querySelector("[name=email]");
 var message = modal.querySelector("[name=message]");
-var isStorageSupportLogin = true;
-var isStorageSupportEmail = true;
-var storageLogin = "";
-var storageEmail = "";
 
 // Переменные карты
 
@@ -16,40 +12,11 @@ var mapLink = document.querySelector(".map-link");
 var map = document.querySelector(".map-modal");
 var mapClose = document.querySelector(".map-close-button");
 
-try {
-    storageLogin = localStorage.getItem("login");
-} catch (err) {
-    isStorageSupport = false;
-}
-
-try {
-    storageEmail = localStorage.getItem("email");
-} catch (err) {
-    isStorageSupportEmail = false;
-}
 
 contactUs.addEventListener("click", function(evt) {
     evt.preventDefault();
     modal.classList.add("modal-on");
-
-    if (storageLogin && !storageEmail) {
-        login.value = storageLogin;
-    } else {
-        email.focus();
-    }
-
-    if (!storageLogin && storageEmail) {
-        email.value = storageEmail;
-    } else {
-        login.focus();
-    }
-
-    if (storageLogin && storageEmail) {
-        login.value = storageLogin;
-        email.value = storageEmail
-    } else {
-        message.focus();
-    }
+    login.focus();
 });
 
 close.addEventListener("click", function(evt) {
@@ -61,16 +28,10 @@ form.addEventListener("submit", function(evt) {
     if (!login.value) {
         evt.preventDefault();
         login.classList.add("invalid");
-    } else if (!email.value) {
+    }
+    if (!email.value) {
         evt.preventDefault();
         email.classList.add("invalid");
-    } else {
-        if (isStorageSupportLogin) {
-            localStorage.setItem("login", login.value);
-        }
-        if (isStorageSupportEmail) {
-            localStorage.setItem("email", email.value);
-        }
     }
 });
 
