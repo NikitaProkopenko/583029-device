78683b9fa7f4a3aaa3e24c59c553af287491fd23
Nikita Prokopenko <guardprokopenko@gmail.com>
2018-03-31 15:37:14 +0300

    Фиксы

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 var contactUs = document.querySelector(".contact-us");
 var modal = document.querySelector(".modal-contact");
 var close = document.querySelector(".modal-close-button");
-var form = modal.querySelector("form");
-var login = modal.querySelector("[name=login]");
-var email = modal.querySelector("[name=email]");
-var message = modal.querySelector("[name=message]");
+
+if (modal) {
+    var form = modal.querySelector("form");
+    var login = modal.querySelector("[name=login]");
+    var email = modal.querySelector("[name=email]");
+    var message = modal.querySelector("[name=message]");
+}
 
 // Переменные карты
 
@@ -12,35 +15,44 @@ var mapLink = document.querySelector(".map-link");
 var map = document.querySelector(".map-modal");
 var mapClose = document.querySelector(".map-close-button");
 
-
-contactUs.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    modal.classList.add("modal-on");
-    login.focus();
-});
-
-close.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    modal.classList.remove("modal-on");
-});
-
-form.addEventListener("submit", function(evt) {
-    if (!login.value) {
+if (contactUs) {
+    contactUs.addEventListener("click", function(evt) {
         evt.preventDefault();
-        login.classList.add("invalid");
-    }
-    if (!email.value) {
+        modal.classList.add("modal-on");
+        login.focus();
+    });
+}
+
+if (close) {
+    close.addEventListener("click", function(evt) {
         evt.preventDefault();
-        email.classList.add("invalid");
-    }
-});
+        modal.classList.remove("modal-on");
+    });
+}
 
-mapLink.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    map.classList.add("modal-on");
-});
+if (form) {
+    form.addEventListener("submit", function(evt) {
+        if (!login.value) {
+            evt.preventDefault();
+            login.classList.add("invalid");
+        }
+        if (!email.value) {
+            evt.preventDefault();
+            email.classList.add("invalid");
+        }
+    });
+}
 
-mapClose.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    map.classList.remove("modal-on");
-});
+if (mapLink) {
+    mapLink.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        map.classList.add("modal-on");
+    });
+}
+
+if (mapClose) {
+    mapClose.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        map.classList.remove("modal-on");
+    });
+}
